17faa47983b823c7f4a1eca5629d28656bc71d1a
SrTobi <code.databyte@gmail.com>
2016-04-18 10:45:04 +0200

    Init

---
+++ YoutubeDownloader/CliOptions.cs
@@ -0,0 +1,26 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeDownloader
+{
+	class CliOptions
+	{
+		[ValueOption(0)]
+		public string YouTubeUrl { get; set; }
+
+		[ValueOption(1)]
+		public string DestFile { get; set; }
+
+		[HelpOption]
+		public string GetUsage()
+		{
+			return
+				"YoutubeDownloader <url> <dest>\n";
+		}
+	}
+}
